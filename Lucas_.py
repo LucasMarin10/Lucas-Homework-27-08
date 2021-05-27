a2f09b6e975e30ed485cdc810b5bfe2f7760489b
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-05-27 18:31:16 -0300

    Add files via upload

---
+++ Lucas_.py
@@ -0,0 +1,46 @@
+#question 2
+automot=["Chevrolet","Hyundai","Volkswagen","Fiat","Ford","Jeep"]
+#question 3
+print(automot[1])
+#question 4
+automot.append("bicycle")
+#question 5
+print(automot)
+#question 6
+automot[0]="Lucas"
+#question 7
+print(automot)
+#question 8
+MeVM=automot[0]+" "+automot[1]
+#question 9
+print(MeVM)
+#question 10
+TV=automot[6].upper()
+#question 11
+print(TV)
+#question 12
+automot.insert(2,"Nova Odessa")
+#question 13
+print(automot)
+#question 14
+del automot[1]
+#question 15
+print(automot)
+#question 16
+autopop=automot.pop()
+#question 17
+print(automot)
+print(autopop)
+#question 18
+M=automot.pop(0)
+#question 19
+print(automot)
+print(M)
+#question 20
+healths=["melon","banana","apple"]
+#question 21
+print(healths)
+#question 22
+healths.sort()
+#question 23
+print(healths)
